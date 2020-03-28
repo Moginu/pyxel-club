e7167e42b7504115a4befb5bee9db99844bbb063
nrookie <228611676@qq.com>
2020-03-28 22:42:54 +0800

    feat(bird):use different image resources in falling and jumping

--- src/app.py
+++ src/app.py
@@ -6,14 +6,19 @@ class App:
         pyxel.init(160, 240, caption="test")
         pyxel.load("assets/jump_game.pyxres")
 
+        # board properties
+        self.board_fps = 30
+
+        # birds properties
         self.bird_x = 65
         self.bird_y = 100
 
         self.bird_is_alive = True
 
-        self.bird_falling_speed = 1
-        self.bird_rising_speed = 10
-
+        self.total_time = 10
+        self.bird_falling_speed = pyxel.height/self.total_time/self.board_fps
+        self.bird_rising_speed = 10*self.bird_falling_speed
+        self.bird_jump = False
 
         pyxel.run(self.update, self.draw)
 
@@ -34,16 +39,18 @@ class App:
 
     def update_bird(self):
         if pyxel.btnp(pyxel.KEY_SPACE):
-            self.bird_y= (self.bird_y-self.bird_rising_speed) %pyxel.height
+            self.bird_y = (self.bird_y-self.bird_rising_speed) % pyxel.height
+            self.bird_jump = True
         else:
             self.bird_y = (self.bird_y+self.bird_falling_speed) % pyxel.height
+            self.bird_jump = False
 
     def draw_bird(self):
         pyxel.blt(
             self.bird_x,
             self.bird_y,
             0,
-            0,
+            16 if self.bird_jump is True else 0,
             0,
             16,
             16,
