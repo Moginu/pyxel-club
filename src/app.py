3792b0e8a18c45dad023e156872815abb74037de
nrookie <228611676@qq.com>
2020-04-18 21:18:46 +0800

    refactor(death_judgment): rewrite the death_judgment function

--- src/app.py
+++ src/app.py
@@ -177,29 +177,37 @@ class App:
             )
 
 
-    def _generate_rectangle(self, x, y, width, height):
-        return [x, y, x + width, y + height]
-
     def death_judgment(self):
-        bird_rec = self._generate_rectangle(self.bird_x, self.bird_y, self.BIRD_WIDTH, self.BIRD_HEIGHT)
-        # print(bird_rec)
-        up_pipe_rec = self._generate_rectangle(self.pipe_pair.up.x, self.pipe_pair.up.y, Pipe.PIPE_WIDTH,
-                                               -self.pipe_pair.up.h)
-        # print(up_pipe_rec)
-        down_pipe_rec = self._generate_rectangle(self.pipe_pair.down.x, self.pipe_pair.down.y, Pipe.PIPE_WIDTH,
-                                                 pyxel.height - self.pipe_pair.up.h - Pipe.PIPE_VERTICAL_GAP)
-        # print(down_pipe_rec)
-        # rec[x1,y1,x2,y2]
-        if not (bird_rec[2] <= up_pipe_rec[0] or  # left
-                bird_rec[3] <= up_pipe_rec[1] or  # bottom
-                bird_rec[0] >= up_pipe_rec[2] or  # right
-                bird_rec[1] >= up_pipe_rec[3]):  # top
-            self.death_event()
-            self.bird_is_alive = False
-        if not (bird_rec[2] <= down_pipe_rec[0] or
-                bird_rec[3] <= down_pipe_rec[1] or
-                bird_rec[0] >= down_pipe_rec[2] or
-                bird_rec[1] >= down_pipe_rec[3]):
+        bird_rec = self._generate_rectangle(
+            self.bird_x,
+            self.bird_y,
+            self.BIRD_WIDTH,
+            self.BIRD_HEIGHT
+        )
+        up_pipe_rec = self._generate_rectangle(
+            self.pipe_pairs[0].up.x,
+            self.pipe_pairs[0].up.y,
+            Pipe.PIPE_WIDTH,
+            -self.pipe_pairs[0].up.h
+        )
+        down_pipe_rec = self._generate_rectangle(
+            self.pipe_pairs[0].down.x,
+            self.pipe_pairs[0].down.y,
+            Pipe.PIPE_WIDTH,
+            pyxel.height - self.pipe_pairs[0].up.h - Pipe.PIPE_VERTICAL_GAP
+        )
+
+        self._check_death(bird_rec, up_pipe_rec)
+        self._check_death(bird_rec, down_pipe_rec)
+
+    def _generate_rectangle(self, x, y, width, height):
+        return [x, y, x+width, y+height]
+
+    def _check_death(self, bird_rec, pipe_rec):
+        if not (bird_rec[2] <= pipe_rec[0]
+           or bird_rec[3] <= pipe_rec[1]
+           or bird_rec[0] >= pipe_rec[2]
+           or bird_rec[1] >= pipe_rec[3]):
             self.death_event()
             self.bird_is_alive = False
 
@@ -207,7 +215,6 @@ class App:
         self.bird_is_alive = False
 
     def draw_death(self):
-        """Draw a blank screen with some text."""
         pyxel.cls(col=0)
         pyxel.text(55, 41, "Game over", pyxel.frame_count % 16)
 
